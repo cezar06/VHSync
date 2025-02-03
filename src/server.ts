362a9212cc9997d86f9993cee2afc9a4597599fe
cezar06 <tudor.cezar01@gmail.com>
2025-02-03 21:32:29 +0200

    Support client connections

--- src/server.ts
+++ src/server.ts
@@ -12,9 +12,22 @@ const io = new Server(server);
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+
+    socket.on('videoAction', (data) => {
+        console.log('Received videoAction:', data);
+        socket.broadcast.emit('videoAction', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Client disconnected: ${socket.id}`);
+    });
 });
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
